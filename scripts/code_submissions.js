65e5c666d219ffe428718a9ff36b305a5b749701
Armaan Shah <armaanshah96@gmail.com>
2018-03-14 18:08:39 -0500

    update code submission script, run output, times, and difference between

--- scripts/code_submissions.js
+++ scripts/code_submissions.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 
-// Receive code string, output beautified code string
-// String -> String
-function beautifySubmissions(code) {
-}
+const LOGS_DIR = 'logs';
 
 // given the correct file, grab each log and output beautified each code submission
 // String -> String
-function getCodeSubmission() {
-  const filepath = 'logs/sanfeng-christian/sanfeng-christian-1.log';
+function printCodeOfFile(filepath) {
+  //const filepath = LOGS_DIR + '/sanfeng-christian/sanfeng-christian-1.log';
   const contents = fs.readFileSync(filepath, { encoding: 'utf-8' });
   const lines = contents.trim().split('\n');
+  let snippetNumber = 0
+  let lastRunTS = 0;
+  let runToSubmitTime = 0;
 
   for (let line of lines) {
     const object = JSON.parse(line);
@@ -24,7 +25,6 @@ function getCodeSubmission() {
     switch (object.data.type) {
       case 'WS_COMMAND':
         if(object.data.command.type === 'COMMAND_RUN_CODE') {
-          //code_string = trimHeader(object.data.command.fn);
           code_string = object.data.command.fn;
         }
         break;
@@ -37,8 +37,31 @@ function getCodeSubmission() {
         break;
     }
 
-    console.log(code_string);
+    if(code_string.length > 0) {
+      lastRunTS = (lastRunTS === 0) ? object.timestamp : lastRunTS;
+      let duration = (object.timestamp - lastRunTS)/1000;
+      runToSubmitTime += duration;
+      snippetNumber++;
+
+      console.log(chalk.yellow(`Start of code snippet ${ snippetNumber }`));
+      console.log(chalk.blue(`${ duration } second since last run`));
+      console.log(code_string)
+
+      lastRunTS = object.timestamp;
+    }
+  }
+  console.log(chalk.magenta(`${ runToSubmitTime } seconds between first run and final submission`));
+}
+
+function printCodeOfFiles(fileDirectory) {
+  const directoryPath = LOGS_DIR + '/' + fileDirectory;
+  const files = fs.readdirSync(directoryPath);
+  for (let file of files) {
+    console.log(`\nStart of ${ chalk.green(file) }\n`);
+    const filepath = path.join(directoryPath + file);
+    printCodeOfFile(path.join(filepath));
+    console.log(`\n\nEnd of ${ chalk.red(file) }\n`);
   }
 }
 
-getCodeSubmission();
+printCodeOfFiles(process.argv[2]);
